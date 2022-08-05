525f20d4b51b46acf3466be50cd8b0429b27b0bc
Daniel Semblano <dsemblano@gmail.com>
2022-08-05 20:57:42 -0300

    gtag fix

--- site/web/app/themes/yurialesi/app/setup.php
+++ site/web/app/themes/yurialesi/app/setup.php
@@ -134,3 +134,12 @@ add_action('widgets_init', function () {
         'id' => 'sidebar-footer',
     ] + $config);
 });
+
+// Add Code to head
+add_action('wp_head', function () {
+    include get_template_directory() . '/resources/views/partials/snippets/gtag.js';
+    include get_template_directory() . '/resources/views/partials/snippets/gconsoleverif.php';
+    include get_template_directory() . '/resources/views/partials/snippets/clarity.php';
+    include get_template_directory() . '/resources/views/partials/snippets/gtagbody.js';
+    include get_template_directory() . '/resources/views/partials/snippets/gtaghead.js';
+}, 100);
