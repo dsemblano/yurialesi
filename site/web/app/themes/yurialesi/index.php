d563356f07b1d614236fc4ee8a92c022e84cf4bc
Daniel Semblano <dsemblano@gmail.com>
2022-09-17 14:52:33 -0300

    favicon

--- site/web/app/themes/yurialesi/index.php
+++ site/web/app/themes/yurialesi/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
+    <?php echo \Roots\view('layouts/head/favicon')->render(); ?>
   </head>
 
   <body <?php body_class(); ?>>
