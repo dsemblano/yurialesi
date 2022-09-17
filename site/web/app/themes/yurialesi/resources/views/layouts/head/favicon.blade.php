2873c863414ddc440fde880b9ddeadcde7c2ded5
Daniel Semblano <dsemblano@gmail.com>
2022-09-17 15:20:48 -0300

    new favicon

--- site/web/app/themes/yurialesi/resources/views/layouts/head/favicon.blade.php
+++ site/web/app/themes/yurialesi/resources/views/layouts/head/favicon.blade.php
@@ -1,11 +1,8 @@
-<link rel="icon" type="image/x-icon" sizes="192x192" href="@asset('images/favicon/favicon.ico')">
-<link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
-<link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+
 <link rel="apple-touch-icon" sizes="180x180" href="@asset('images/favicon/apple-touch-icon.png')">
-<link rel="icon" type="image/png" sizes="192x192" href="@asset('images/favicon/android-chrome-192x192')">
-<link rel="icon" type="image/png" sizes="512x512" href="@asset('images/favicon/android-chrome-512x512.png')">
+<link rel="icon" type="image/png" sizes="32x32" href="@asset('images/favicon/favicon-32x32.png')">
+<link rel="icon" type="image/png" sizes="16x16" href="@asset('images/favicon/favicon-16x16.png')">
 <link rel="manifest" href="@asset('images/favicon/site.webmanifest')">
-{{-- <meta name="msapplication-config" content="@asset('images/favicon/browserconfig.xml')"> --}}
-{{-- <link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg" color="#5bbad5')"> --}}
-{{-- <meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff"> --}}
+<link rel="mask-icon" href="@asset('images/favicon/safari-pinned-tab.svg')" color="#163b24">
+<meta name="msapplication-TileColor" content="#163b24">
+<meta name="theme-color" content="#ffffff">
